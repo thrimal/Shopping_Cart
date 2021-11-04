6b4de66aaba9625b6ea47843de7d9cac066abea3
Thrimal <thrimalavishka99@gmail.com>
2021-11-04 12:42:52 +0530

    final

--- FrontEnd_With_Angular/Shopping-Cart/src/app/app-routing.module.ts
+++ FrontEnd_With_Angular/Shopping-Cart/src/app/app-routing.module.ts
@@ -5,12 +5,11 @@ import {ProductsComponent} from "./components/products/products.component";
 import {PlaceOrderComponent} from "./components/placeOrder/placeOrder.component";
 import {OrderDetailComponent} from "./components/order-detail/order-detail.component";
 import {TestComponent} from "./components/test/test.component";
-import {AuthGuardGuard} from "./authguard/auth-guard.guard";
 
 const routes: Routes = [
   {path:'',redirectTo:"customer",pathMatch:"full"},
   {path:'customer',component:CustomerComponent},
-  {path:'products',component:ProductsComponent,canActivate:[AuthGuardGuard]},
+  {path:'products',component:ProductsComponent},
   {path:'placeOrder',component:PlaceOrderComponent},
   {path:'orderDetail',component:OrderDetailComponent},
   {path:'test',component:TestComponent},
